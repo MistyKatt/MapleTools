2afee9f58e53abf6a55e1beede802be09f2f4639
mistykat <lvhaolin1994@gmail.com>
2025-03-06 00:29:23 -0500

    1. setup static file folder structure 2. beautify banlist page

---
+++ MapleTools/wwwroot/js/Home/BanList/banlist.js
@@ -0,0 +1,31 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    // Initialize all collapses as open
+    $('.collapse').collapse('show');
+    $('#toggleCollapse').text("hide all");
+});
+
+// Handle toggle rotation
+$('[data-toggle="collapse"]').on('click', function () {
+    $(this).toggleClass('collapsed');
+});
+
+// Remove accordion behavior
+$('.collapse').on('show.bs.collapse', function () {
+    $(this).parent().find('.collapse').collapse('hide');
+});
+
+$('#toggleCollapse').click(function () {
+    if ($(this).hasClass('expanded')) {
+        $(this).removeClass('expanded');
+        $(this).text("show all");
+        $('.collapse').collapse('hide');
+        $('[data-toggle="collapse"]').addClass('collapsed');
+    }
+    else {
+        $(this).addClass('expanded');
+        $(this).text("hide all");
+        $('.collapse').collapse('show');
+        $('[data-toggle="collapse"]').removeClass('collapsed');
+    }
+});
+
